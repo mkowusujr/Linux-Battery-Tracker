5bfb417a3dd163c010ac9a613111639dede2387b
mkowusujr <mrfriiday@gmail.com>
2021-12-18 00:33:12 -0500

    Added some more queue functions

--- queue.h
+++ queue.h
@@ -1,12 +1,21 @@
-// file:
-// description:
-// author: mathew owusu
+/// file:
+/// description:
+/// author: mathew owusu
 
-//
+///
 typedef struct queue_s * Queue;
 
-//
+///
 void enqueue();
 
-//
+///
 void dequeue();
+
+///
+void* qfront();
+
+///
+void* qrear();
+
+///
+int qlength();
