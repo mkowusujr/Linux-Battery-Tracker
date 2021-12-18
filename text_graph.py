c35dd0ef0dce51bb21479e1dd2525308520a1325
mkowusujr <mrfriiday@gmail.com>
2021-12-18 00:24:13 -0500

    Added comments

--- text_graph.py
+++ text_graph.py
@@ -1,6 +1,6 @@
 """
-file: Handling the graphing of the battery tracker
-description:
+file:
+description: Handling the graphing of the battery tracker
 author: mathew owusu jr
 """
 import matplotlib.pyplot as plt
@@ -9,22 +9,35 @@ import datetime
 
 
 def main():
+    """
+
+    """
     times = []
     battery = []
+
+    # Open file
     with open("/var/lib/bat_data") as file:
         for line in file:
+            # Break it apart
             line = line.rstrip()
             data = line.split(",")
 
+            # store time data
             val = int(data[0])
             time = datetime.datetime.fromtimestamp(val)
             times.append(time)
 
+            # store battery data
             battery.append(float(data[1]))
     file.close()
+
+    # store data in coordinates
     x_vals = np.array(times)
     y_vals = np.array(battery)
+
+    # plot the coordinates
     plt.plot(x_vals, y_vals)
+    # plt.gca()
     plt.show()
 
 
