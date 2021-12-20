3a6235aee1fc99502ef7b9ef893dbc7a9b846609
mkowusujr <mrfriiday@gmail.com>
2021-12-20 13:01:13 -0500

    Output format changed

--- time_as_string_test.c
+++ time_as_string_test.c
@@ -7,11 +7,16 @@ int main()
     // store current time as a string
     time_t curr_time = time(0);
 	char *time_string = ctime(&curr_time);
-    time_string[strlen(time_string) - 1] = 0;
+    time_string[strlen(time_string) - 1] = 0; // removes '\n' 
+    for (int i = 0; i < strlen(time_string); i++)
+    {
+        if (time_string[i] == ' ')
+            time_string[i] = '_';
+    }
     // create a file with that name
     FILE *target = fopen(time_string, "w");
     // write to that file and close
-	fprintf(target, "%s", time_string);
+	fprintf(target, "%lu\n", curr_time);
 	fclose(target);
 	return 0;
 }
