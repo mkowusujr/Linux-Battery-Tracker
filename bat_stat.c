ab150b6a53cf05a19e9b4797f3f879ca5c1e3406
mkowusujr <mrfriiday@gmail.com>
2021-12-20 13:36:10 -0500

    Added function for generating new filenames

--- bat_stat.c
+++ bat_stat.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
+#include "queue.h"
 
 // Location of current battery voltage file 
 #define ENERGY_NOW "/sys/class/power_supply/BAT0/energy_now"
@@ -20,7 +22,24 @@
 #define TWELVE_O_ONE 18060
 
 
-// This function
+// Function to create name of past day file
+//
+// @param:
+// @reutrn:
+char *new_filename(time_t curr_time)
+{
+    char *time_string = ctime(&curr_time);
+    time_string[strlen(time_string) - 1] = 0; // removes '\n' 
+    for (int i = 0; i < strlen(time_string); i++)
+    {
+        if (time_string[i] == ' ')
+            time_string[i] = '_';
+    }
+    return time_string;
+}
+
+
+// This function has the main loop
 //
 int main(void)
 {
@@ -34,8 +53,13 @@ int main(void)
     // Creating file pointers
     FILE *cur_bat, *max_bat, *output;
     
-    // Main loop
+    // queue for handling past bat day data
+    Queue past_days = make_queue(5);
+
+    // Have I cleared the current file?
     int clear_file = 0;
+
+    // main loop of daemon process
     while(1)
     {
         // Read current battery
@@ -64,6 +88,19 @@ int main(void)
                 ((cur_time % UNIX_DAY >= MIDNIGHT) && 
                  (cur_time % UNIX_DAY <= TWELVE_O_ONE)))
         {
+            // Add current file to log queue
+            FILE *source = fopen(BAT_DATA,"r");
+            char *nameee= (char*)cur_time;
+
+            FILE *target = fopen(nameee,"w");
+            // copy over file contents
+            fprintf(target, "bruh\n");
+            fclose(target); 
+            // enqueue target file
+            enqueue(past_days, (void*)nameee);
+            fclose(source);
+
+            // Wipe current file
             output = fopen(BAT_DATA, "w");
             fprintf(output, "%lu,%0.2f\n", cur_time, bat_percentage);
             clear_file = 1;
@@ -76,6 +113,7 @@ int main(void)
         
         if (cur_time % UNIX_DAY > TWELVE_O_ONE)
             clear_file = 0;
+        
         // Closing files, freeing memory
         fclose(cur_bat);
         fclose(max_bat);
