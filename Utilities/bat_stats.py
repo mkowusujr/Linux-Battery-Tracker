ee82c231f2393af4650c66737abf2a42c3c9f4e0
mkowusujr <mrfriiday@gmail.com>
2021-12-22 18:22:36 -0500

    Basic Gtk Layout Created, buttons do nothing, no graphing yet

--- Utilities/bat_stats.py
+++ Utilities/bat_stats.py
@@ -3,9 +3,20 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
+class LBT_gui:
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("lbt_gtk_layout.glade")
+        self.builder.connect_signals(self)
 
-win = Gtk.Window(title="Linux Battery Tracker")
-win.show()
-win.connect("destroy", Gtk.main_quit)
+        self.window = self.builder.get_object("main_win")
+        self.window.show_all()
 
-Gtk.main()
+if __name__ == '__main__':
+    LBT_gui()
+    Gtk.main()
+# win = Gtk.Window(title="Linux Battery Tracker")
+# win.show()
+# win.connect("destroy", Gtk.main_quit)
+#
+# Gtk.main()
