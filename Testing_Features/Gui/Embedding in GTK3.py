1a9f3667b9d81670416704f9b0cce5dc50021b28
mkowusujr <mrfriiday@gmail.com>
2021-12-22 22:03:04 -0500

    Successfully graphs battery life on current day

--- Testing_Features/Gui/Embedding in GTK3.py
+++ Testing_Features/Gui/Embedding in GTK3.py
@@ -7,7 +7,38 @@ from matplotlib.backends.backend_gtk3agg import (
     FigureCanvasGTK3Agg as FigureCanvas)
 from matplotlib.figure import Figure
 import numpy as np
+import datetime
 
+
+def make_fig(file):
+    fig = Figure(figsize=(5, 4), dpi=100)
+    ax = fig.add_subplot()
+
+    times = []
+    battery = []
+    # Open file
+    with open(file, 'r') as data:
+        for line in data.readlines():
+            # Break it apart
+            line = line.rstrip()
+            line = line.split(",")
+
+            # store time data
+            val = int(line[0])
+            time = datetime.datetime.fromtimestamp(val)
+            times.append(time)
+
+            # store battery data
+            battery.append(float(line[1]))
+    data.close()
+
+    # store data in coordinates
+    x_vals = np.array(times)
+    y_vals = np.array(battery)
+
+    # plot the coordinates
+    ax.plot(x_vals, y_vals)
+    return fig
 
 # Creating gtk window
 win = Gtk.Window()
@@ -15,13 +46,13 @@ win.connect("delete-event", Gtk.main_quit)
 win.set_default_size(1000, 600)
 win.set_title("Embedding in GTK3")
 
-
 # Plotting data to a figure
-fig = Figure(figsize=(5, 4), dpi=100)
-ax = fig.add_subplot()
-t = np.arange(0.0, 3.0, 0.01)
-s = np.sin(2*np.pi*t)
-ax.plot(t, s)
+# fig = Figure(figsize=(5, 4), dpi=100)
+# ax = fig.add_subplot()
+# t = np.arange(0.0, 3.0, 0.01)
+# s = np.sin(2*np.pi*t)
+# ax.plot(t, s)
+fig = make_fig("/var/lib/bat_data")
 
 # sw = Gtk.ScrolledWindow()
 # win.add(sw)
--- Utilities/linux_batter_tracker_gtk.py
+++ Utilities/linux_batter_tracker_gtk.py
@@ -19,21 +19,51 @@ class LBT_gui:
 
         self.window = self.builder.get_object("main_win")
         self.window.show_all()
-        self.plot_data()
+        self.plot_data("/var/lib/bat_data")
 
-
-    def plot_data(self):
-        canvas_window = self.builder.get_object("main_win_display")
-        # graph
+    def make_fig(self, file):
         fig = Figure(figsize=(5, 4), dpi=100)
         ax = fig.add_subplot()
-        t = np.arange(0.0, 3.0, 0.01)
-        s = np.sin(2 * np.pi * t)
-        ax.plot(t, s)
 
+        times = []
+        battery = []
+        # Open file
+        with open(file, 'r') as data:
+            for line in data.readlines():
+                # Break it apart
+                line = line.rstrip()
+                line = line.split(",")
+
+                # store time data
+                val = int(line[0])
+                time = datetime.datetime.fromtimestamp(val)
+                times.append(time)
+
+                # store battery data
+                battery.append(float(line[1]))
+        data.close()
+
+        # store data in coordinates
+        x_vals = np.array(times)
+        y_vals = np.array(battery)
+
+        # plot the coordinates
+        ax.plot(x_vals, y_vals)
+        return fig
+
+
+    def plot_data(self, file):
+        canvas_window = self.builder.get_object("main_win_display")
+        # graph
+        # fig = Figure(figsize=(5, 4), dpi=100)
+        # ax = fig.add_subplot()
+        # t = np.arange(0.0, 3.0, 0.01)
+        # s = np.sin(2 * np.pi * t)
+        # ax.plot(t, s)
+        fig = self.make_fig(file)
         #
         canvas = FigureCanvas(fig)
-        canvas.set_size_request(1005, 400)
+        canvas.set_size_request(900, 500)
         canvas_window.add(canvas)
         canvas_window.show_all()
 
