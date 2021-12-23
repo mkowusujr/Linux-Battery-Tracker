b39ea2c1552915ecfb08c1ac94a6502015f3d018
mkowusujr <mrfriiday@gmail.com>
2021-12-22 22:41:11 -0500

    All buttons workings :)

--- Utilities/linux_batter_tracker_gtk.py
+++ Utilities/linux_batter_tracker_gtk.py
@@ -5,11 +5,11 @@ from matplotlib.backends.backend_gtk3agg import (
     FigureCanvasGTK3Agg as FigureCanvas)
 from matplotlib.figure import Figure
 import numpy as np
-# TODO set up time
 import datetime
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+
 
 class LBT_gui:
     def __init__(self):
@@ -20,6 +20,17 @@ class LBT_gui:
         self.window = self.builder.get_object("main_win")
         self.window.show_all()
         self.plot_data("/var/lib/bat_data")
+
+
+    def fetch_bat_log(self):
+        # list_of_days = []
+        with open("/home/mathew/Local_GitHub_Repositories/Linux-Battery-Tracker/Created_Data/bat_log.txt", 'r') as log:
+            days = log.readline().strip()
+            days = days.split(',')
+            list_of_days = days
+        log.close()
+        return list_of_days
+
 
     def make_fig(self, file):
         fig = Figure(figsize=(5, 4), dpi=100)
@@ -54,18 +65,52 @@ class LBT_gui:
 
     def plot_data(self, file):
         canvas_window = self.builder.get_object("main_win_display")
-        # graph
-        # fig = Figure(figsize=(5, 4), dpi=100)
-        # ax = fig.add_subplot()
-        # t = np.arange(0.0, 3.0, 0.01)
-        # s = np.sin(2 * np.pi * t)
-        # ax.plot(t, s)
+        # graph data
         fig = self.make_fig(file)
-        #
+        # add to canvas and window
         canvas = FigureCanvas(fig)
         canvas.set_size_request(900, 500)
         canvas_window.add(canvas)
         canvas_window.show_all()
+
+
+    def on_td_clicked(self, button):
+        days = self.fetch_bat_log()
+        self.plot_data("/var/lib/bat_data")
+
+
+    def on_yd_clicked(self, button):
+        days = self.fetch_bat_log()
+        self.plot_data(days[0])
+
+
+    def on_2d_clicked(self, button):
+        days = self.fetch_bat_log()
+        self.plot_data(days[1])
+
+
+    def on_3d_clicked(self, button):
+        days = self.fetch_bat_log()
+        self.plot_data(days[2])
+
+
+    def on_4d_clicked(self, button):
+        days = self.fetch_bat_log()
+        self.plot_data(days[3])
+
+
+    def on_5d_clicked(self, button):
+        days = self.fetch_bat_log()
+        self.plot_data(days[4])
+
+
+    def on_6d_clicked(self, button):
+        days = self.fetch_bat_log()
+        self.plot_data(days[5])
+
+    def on_wk_clicked(self, button):
+        days = self.fetch_bat_log()
+        self.plot_data(days[6])
 
 if __name__ == '__main__':
     LBT_gui()
